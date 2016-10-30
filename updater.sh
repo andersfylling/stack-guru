c4bec4aa82449277a5ce30001193d70c8d2efdaa
Anders Øen Fylling <chew-exit-2060@opayq.com>
2016-10-30 22:03:48 +0100

    added sudo on kill

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ git pull
 
 if ps -p $(ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}') > /dev/null
 then
-    kill $(ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}')
+    sudo kill $(ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}')
 fi
 
 nohup php bot.bootstrap.php &
