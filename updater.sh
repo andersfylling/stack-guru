419ca78a0903ae8f5fd444df98f287c4bc976eab
Anders Øen Fylling <chew-exit-2060@opayq.com>
2016-10-30 21:54:48 +0100

    updated updator

--- updater.sh
+++ updater.sh
@@ -2,10 +2,9 @@
 
 git pull
 
-pidID = ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}'
-if ps -p $pidID > /dev/null
+if ps -p $(ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}') > /dev/null
 then
-    kill $pidID
+    kill $(ps aux | grep '[p]hp bot.bootstrap.php' | awk '{print $2}')
 fi
 
 nohup php bot.bootstrap.php &
