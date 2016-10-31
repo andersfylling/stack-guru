ff42cae0a378bb984f0cd47ae32ac8229c6b9ae5
Anders Øen Fylling <chew-exit-2060@opayq.com>
2016-10-31 01:16:30 +0100

    awesome command

---
+++ implementations/Awesome.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Just for giggles.
+ *
+ * @author http://github.com/sciencefyll
+ */
+
+namespace Commands;
+
+
+class Awesome implements Command
+{
+
+    private $description;
+    private $help;
+
+
+    public function __construct()
+    {
+        $this->description  = Command::defaults["description"];
+        $this->help         = Command::defaults["help"];
+    }
+
+    public function command ($args, $in)
+    {
+        $in->reply("That's awesome.");
+    }
+
+    public function linkDiscordObject ($callback)
+    {
+        //$this->discord = $callback();
+    }
+
+    public function getDescription() : string
+    {
+        return $this->description;
+    }
+
+    public function getHelp() : string
+    {
+        return $this->help;
+    }
+
+}
