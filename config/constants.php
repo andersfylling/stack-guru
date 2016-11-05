1eb80d8a51b6bb27afb1f47a7b036e4ce86f409c
Anders Øen Fylling <fyllingz@gmail.com>
2016-11-05 20:45:25 +0100

    const file

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This allows for unit testing.
+ * Should be set to false when added to master.
+ *
+ * @see \StackGuru\CoreLogic\Utils\Response Response class uses this to echo replies rather than sending them.
+ */
+const DEVELOPMENT = true;
