7720d250785bfe14e4f050a9e3aad85f9f75c62d
Anders Øen Fylling <fyllingz@gmail.com>
2016-11-05 18:58:47 +0100

    Added extra option to verify command

--- src/CoreLogic/Utils/ResolveOptions.php
+++ src/CoreLogic/Utils/ResolveOptions.php
@@ -3,6 +3,7 @@
 namespace StackGuru\CoreLogic\Utils;
 
 use \Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
+use Symfony\Component\OptionsResolver\Exception\NoSuchOptionException;
 
 /**
  * Class ResolveOptions
@@ -19,13 +20,29 @@ class ResolveOptions
      *
      * @param array $options Elements to verify/confirm has the $requirements
      * @param array $requirements Elements that must exist within the $options class
+     * @param bool $keysMustBeString
      * @return array $options Nothing gets changed. But gives a nicer syntax. Potentially more to come.
      *
-     * @throws MissingOptionsException
      * @see \Symfony\Component\OptionsResolver\Exception\MissingOptionsException
+     * @see \Symfony\Component\OptionsResolver\Exception\NoSuchOptionException
      */
-    public static function verify (array $options = [], array $requirements = []) : array
+    public static function verify (
+        array $options = [],
+        array $requirements = [],
+        boolean $keysMustBeString = null
+    ) : array
     {
+        /*
+         * make sure each key is a string.
+         */
+        if ($keysMustBeString !== null && $keysMustBeString === true) {
+            foreach ($options as $key => $value) {
+                if (!is_string($key)) {
+                    throw new NoSuchOptionException("Keys in array cannot be of any type but strings");
+                }
+            }
+        }
+
         /*
          * Loop through each requirement and make sure it exists as a key within the $options array
          */
@@ -33,9 +50,9 @@ class ResolveOptions
             if (!key_exists($key, $options)) { // || !$options[$key] instanceof $value) {
 
                 /*
-                 * Key didn't exist soa  MissingOptionsException is thrown
+                 * Key didn't exist so a MissingOptionsException is thrown
                  */
-                throw new MissingOptionsException("Key file must be specified (and contain a value): {$key}");
+                throw new MissingOptionsException("Key must be specified: {$key}");
             }
         }
 
