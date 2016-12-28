1fded829b4a83e877563510e9a1c512cee3d4c28
Anders Øen Fylling <fyllingz@gmail.com>
2016-12-28 06:32:02 +0100

    Commands can now be implemented

--- stack-guru.php
+++ stack-guru.php
@@ -39,47 +39,72 @@ $bot = new \StackGuru\CoreLogic\Bot([
 /*
  * Event handlers for different states
  */
-$messages_all_including_bot = function () {
+$messages_all_including_bot     = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
     /*
      * Stuff to be called in this state.
      */
 };
 
-$messages_all_excluding_bot = function () {
+$messages_all_excluding_bot     = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
     /*
      * Stuff to be called in this state.
      */
 };
 
-$messages_from_bot = function () {
+$messages_from_bot              = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
     /*
      * Stuff to be called in this state.
      */
 };
 
-$messages_bot_to_bot = function () {
+$messages_bot_to_bot            = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
     /*
      * Stuff to be called in this state.
      */
 };
 
-$messages_other_to_bot = function () {
+$messages_other_to_bot          = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
     /*
      * Stuff to be called in this state.
      */
 };
 
+
+$messages_other_to_bot_ready    = function (
+        \Discord\Parts\Channel\Message $message,
+        array $command = null
+) {
+    /*
+     * Stuff to be called in this state.
+     */
+};
 
 
 
 /*
  * Add listeners
  */
-$bot->state(BotEvent::MESSAGE_ALL_I_SELF,       $messages_all_including_bot);
-$bot->state(BotEvent::MESSAGE_ALL_E_SELF,       $messages_all_excluding_bot);
-$bot->state(BotEvent::MESSAGE_FROM_SELF,        $messages_from_bot);
-$bot->state(BotEvent::MESSAGE_SELF_TO_SELF,     $messages_bot_to_bot);
-$bot->state(BotEvent::MESSAGE_OTHERS_TO_SELF,   $messages_other_to_bot);
+$bot->state(BotEvent::MESSAGE_ALL_I_SELF,           $messages_all_including_bot);
+$bot->state(BotEvent::MESSAGE_ALL_E_SELF,           $messages_all_excluding_bot);
+$bot->state(BotEvent::MESSAGE_FROM_SELF,            $messages_from_bot);
+$bot->state(BotEvent::MESSAGE_SELF_TO_SELF,         $messages_bot_to_bot);
+$bot->state(BotEvent::MESSAGE_OTHERS_TO_SELF,       $messages_other_to_bot);
+$bot->state(BotEvent::MESSAGE_OTHERS_TO_SELF_READY, $messages_other_to_bot_ready);
 
 /*
  * Run the bot
