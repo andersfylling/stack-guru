3aa0cf171c87387a12e4f1c7ee319f5f18a614dc
Anders Øen Fylling <fyllingz@gmail.com>
2017-01-17 01:10:07 +0100

    Prettified version checking

--- stack-guru.php
+++ stack-guru.php
@@ -6,15 +6,12 @@
  * Time: 22.57
  */
 
- define("MIN_PHP_VERSION", "7.1.0");
-
  // Make sure that the php version is correct
- $version = explode('.', MIN_PHP_VERSION);
- define('MIN_PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
- if (PHP_VERSION_ID < MIN_PHP_VERSION_ID) {
-     echo "Your php version is too old: ", phpversion(), PHP_EOL;
-     echo "Please upgrade to version or higher: ", MIN_PHP_VERSION, PHP_EOL;
-     exit;
+ const MIN_PHP_VERSION = "7.1.0";
+ if (version_compare(PHP_VERSION, MIN_PHP_VERSION) < 0) {
+    echo "Your php version is too old: ", phpversion(), PHP_EOL;
+    echo "Please upgrade to version or higher: ", MIN_PHP_VERSION, PHP_EOL;
+    exit;
  }
 
 // Handle incomming terminal arguments.
