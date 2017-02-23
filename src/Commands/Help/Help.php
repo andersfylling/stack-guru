441691f7d17cc2de44273eb4149f044f1692e7ab
Anders Øen Fylling <fyllingz@gmail.com>
2017-02-23 03:47:52 +0100

    You can now show help about a command and subcommand

--- src/Commands/Help/Help.php
+++ src/Commands/Help/Help.php
@@ -5,6 +5,7 @@ namespace StackGuru\Commands\Help;
 use StackGuru\Core\Command\AbstractCommand;
 use StackGuru\Core\Command\CommandContext;
 use StackGuru\Core\Command\CommandEntry;
+use StackGuru\Core\Utils\StringParser;
 
 
 class Help extends AbstractCommand
@@ -15,10 +16,107 @@ class Help extends AbstractCommand
 
     public function process(string $query, ?CommandContext $ctx): string
     {
-        // Encapsulate command list in code block
-        $helptext = "```markdown\n";
+        // all the commands
+        $commands = $ctx->cmdRegistry->getCommands();
 
-        $helptext .= "# Available commands\n";
+        // Encapsulate command list in code block
+        $helptext = "```markdown" . PHP_EOL;
+
+        // Decide if all commands should be shown, just one command or a subcommand.
+        // also mention how a service is used.
+        
+        $keys = StringParser::getFirstWords($query, 2);
+
+        // a sub command
+        if (isset($keys[1]) && isset($commands[$keys[0]]) && isset($commands[$keys[0]]->getChildren()[$keys[1]])) {
+            $command = $commands[$keys[0]];
+            $subcommand = $command->getChildren()[$keys[1]];
+
+
+            $helptext .= "# Command name" . PHP_EOL;
+            $helptext .= "* {$command->getName()} {$subcommand->getName()}" . PHP_EOL . PHP_EOL;
+
+            self::showCommandUsage($helptext, [$subcommand->getName()]);
+
+            $helptext .= "# Description" . PHP_EOL;
+            $helptext .= "* {$subcommand->getDescription()}" . PHP_EOL . PHP_EOL;
+        } 
+
+        // A main command
+        else if (isset($keys[0]) && isset($commands[$keys[0]])) {
+            $command = $commands[$keys[0]];
+
+
+            $helptext .= "# Command name" . PHP_EOL;
+            $helptext .= "* {$command->getName()}" . PHP_EOL . PHP_EOL;
+
+            self::showCommandUsage($helptext, [$command->getName()]);
+
+            $helptext .= "# Description" . PHP_EOL;
+            $helptext .= "* {$command->getDescription()}" . PHP_EOL . PHP_EOL;
+
+
+            // Print all subcommands
+            $helptext .= "# Sub-commands" . PHP_EOL;
+            $children = $command->getChildren();
+            foreach ($children as $name => $child) {
+
+                // Print command with subcommand tree
+                $cmdline = "* !{$command->getName()} {$child->getName()}";
+
+                // Pad command names to align command descriptions
+                $cmdline = sprintf("%-20s", $cmdline);
+
+                // Add command description
+                $description = $child->getDescription();
+                if (!empty($description)) {
+                    $cmdline .= " - {$description}";
+                }
+
+                $helptext .= $cmdline . PHP_EOL;
+            }
+
+
+        } 
+        
+
+        // default, all commands
+        else {
+            self::showCommandUsage($helptext, ["command", "subcommand"]);
+
+            self::showMainCommands($helptext, $ctx);
+        }
+        
+
+
+
+        $helptext .= "```";
+
+        return $helptext;
+    }
+
+    private static function showCommandUsage(string &$helptext, $arr) : void 
+    {
+        // Show command/subcommand syntax.
+        $helptext .= "# Usage" . PHP_EOL;
+
+        for ($i = 0; $i < sizeof($arr); $i++) {
+
+            $helptext .= "* !";
+
+            for ($j = 0; $j <= $i; $j++) {
+                $helptext .= $arr[$j] . ' ';
+            }
+
+            $helptext .= "param1 param2 ... paramN" . PHP_EOL;
+        }
+
+        $helptext .= PHP_EOL;
+    }
+
+    private static function showMainCommands(string &$helptext, ?CommandContext $ctx) : void 
+    {
+        $helptext .= "# Available commands" . PHP_EOL;
 
         // Print all commands
         $commands = $ctx->cmdRegistry->getCommands();
@@ -26,10 +124,11 @@ class Help extends AbstractCommand
             $commandTree = self::getCommandTreeString($command);
 
             // Print command with subcommand tree
-            $cmdline = "* !{$commandTree}";
+            
+            $cmdline = "* !{$command->getName()}";
 
             // Pad command names to align command descriptions
-            $cmdline = sprintf("%-40s", $cmdline);
+            $cmdline = sprintf("%-20s", $cmdline);
 
             // Add command description
             $description = $command->getDescription();
@@ -39,14 +138,11 @@ class Help extends AbstractCommand
 
             $helptext .= $cmdline . "\n";
         }
-        $helptext .= "```";
-
-        return $helptext;
     }
 
     private static function getCommandTreeString(CommandEntry $command): string
     {
-        $str = $command->getName();
+        $str = "";
         $children = $command->getChildren();
         if (sizeof($children) > 0) {
             $subtrees = [];
