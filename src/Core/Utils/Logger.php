e8260e439ed70718b46c6776098ea806e12c6903
Anders Øen Fylling <fyllingz@gmail.com>
2017-03-17 00:37:07 +0100

    updated logger... to start bot..

--- src/Core/Utils/Logger.php
+++ src/Core/Utils/Logger.php
@@ -4,51 +4,23 @@ declare(strict_types=1);
 namespace StackGuru\Core\Utils;
 
 
-class Logger extends StackGuru\Core\Database
+class Logger
 {
+	private static $database = null;
 
-	private $classname = "";
-
-	protected static $path = __DIR__ . "/logger_logs"; // DEFAULT
-
-
-
-	public function __construct(string $name) 
+	private static setDatabase(\PDO $database) 
 	{
-		$this->classname = $name; // should also store namespace somehow. :/
+		static::$database = $database;
 	}
 
 
-	public static function setPath(string $fullPath) 
+	public function callbackExceptionLog(?\Exception $e = null) 
 	{
-		self::$path = $fullPath;
-	}
+		if (null === $object) {
+			return;
+		}
 
-	/**
-	 * Add a log entry to a file.
-	 * 
-	 * @param  string $file  [full path, not relative as it might cause weirdo issues.]
-	 * @param  string $entry [Log entry to be written to log file.]
-	 * @return [boolean]     [true if successfully written to file. Based on return value of file_put_contents.]
-	 */
-	public function appendToLogfile(string $entry) : bool
-	{
-		// ----- FILE BASED VERSION -----
-		// Add new line to end of the entry.
-		//$entry .= PHP_EOL;
-		
-		// Write the contents to the file, 
-		// using the FILE_APPEND flag to append the content to the end of the file
-		// and the LOCK_EX flag to prevent anyone else writing to the file at the same time.
-		//$result = file_put_contents(self::$path . '/' . $this->classname . ".log", $entry, FILE_APPEND | LOCK_EX);
 
-		// file_put_contents() returns the number of bytes that were written to the file, 
-		// or FALSE on failure.
-		//return false === $result ? false : true;
-		// ----- END ------
-		
-		$ 
-		
 	}
 
 
