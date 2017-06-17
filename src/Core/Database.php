c90a0d16b04c1575133e7ca405a4e4e1dede3f82
Anders Øen Fylling <sciencefyll@users.noreply.github.com>
2017-06-17 02:03:47 +0200

    made sql connection persistent

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -32,10 +32,16 @@ class Database
         /*
          * establish a new PDO connection
          */
-        $this->db = new PDO(
-            "mysql:host={$options['host']};port={$options['port']}", $options["user"], $options["pass"],
-            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
-        );
+        //$this->db = new PDO(
+        //    "mysql:host={$options['host']};port={$options['port']}", $options["user"], $options["pass"],
+        //    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+        //);
+        $this->db = new PDO("mysql:host={$options['host']};port={$options['port']}", $options["user"], $options["pass"], 
+            array( 
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'", 
+                PDO::ATTR_PERSISTENT => true 
+                ) 
+        );  
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         /*
